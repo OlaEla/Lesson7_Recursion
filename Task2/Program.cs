5b9ded340e950964260c910739fec804f4eef347
Oleg Elagin <rasdat@yandex.ru>
2023-12-25 19:04:58 +0400

    урок 7 рекурсия задания 1-3

---
+++ Task2/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 2: 
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Задаём значения m и n
+        int m = 2;
+        int n = 5;
+
+        // Вызываем функцию Аккермана и выводим результат
+        int result = AckermannFunction(m, n);
+        Console.WriteLine($"Результат функции Аккермана для m={m}, n={n}: \nA={result}");
+    }
+
+    // Рекурсивная функция для вычисления функции Аккермана
+    static int AckermannFunction(int m, int n)
+    {
+        if (m == 0)
+        {
+            return n + 1;
+        }
+        else if (m > 0 && n == 0)
+        {
+            return AckermannFunction(m - 1, 1);
+        }
+        else if (m > 0 && n > 0)
+        {
+            return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+        }
+
+        return 0; // Этот возврат никогда не выполнится, так как все возможные случаи рассмотрены ранее в функции
+                 // Если происходит выполнение строки, это является индикатором ошибки в логике программы
+    }
+}
+
+
+
+
+
+
---
+++ Task3/Program.cs
@@ -0,0 +1,87 @@
+﻿// Задача 3: 
+// Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите длину массива: ");
+        int length = int.Parse(Console.ReadLine());
+
+        int[] myArray = new int[length];
+
+        // Генерация массива случайных чисел
+        GenerateRandomArray(myArray, 0);
+
+        Console.WriteLine("Заданный массив:");
+        PrintArray(myArray, 0);
+
+        Console.WriteLine("Элементы массива в обратном порядке:");
+        PrintArrayReverse(myArray, 0, myArray.Length - 1);
+
+        //Console.ReadLine(); // Программа завершается без ожидания ввода пользователя
+    }
+
+    // Рекурсивная функция для генерации массива случайных чисел
+
+    // В реальных проектах использование рекурсии для генерации случайных массивов 
+    // может быть неэффективным из-за увеличения глубины стека вызовов и, 
+    // как следствие, возможных проблем с производительностью и использованием памяти
+
+
+    static void GenerateRandomArray(int[] array, int index)
+    {
+        // Если индекс выходит за пределы массива, завершаем рекурсию
+        if (index == array.Length)
+        {
+            return;
+        }
+
+        // Генерируем случайное число и присваиваем его элементу массива
+        Random random = new Random();
+        array[index] = random.Next(1, 100);
+
+        // Рекурсивный вызов для следующего элемента
+        GenerateRandomArray(array, index + 1);
+    }
+
+    // Рекурсивная функция для вывода элементов массива
+    static void PrintArray(int[] array, int index)
+    {
+        // Если индекс выходит за пределы массива, завершаем рекурсию
+        if (index == array.Length)
+        {
+            Console.WriteLine();
+            return;
+        }
+
+        // Выводим текущий элемент
+        Console.Write(array[index] + " ");
+
+        // Рекурсивный вызов для следующего элемента
+        PrintArray(array, index + 1);
+    }
+
+    // Рекурсивная функция для вывода элементов массива в обратном порядке
+    static void PrintArrayReverse(int[] array, int startIndex, int endIndex)
+    {
+        // Если начальный индекс превышает конечный, завершаем рекурсию
+        if (startIndex > endIndex)
+        {
+            Console.WriteLine();
+            return;
+        }
+
+        // Выводим текущий элемент
+        Console.Write(array[endIndex] + " ");
+
+        // Рекурсивный вызов для предыдущего элемента
+        PrintArrayReverse(array, startIndex, endIndex - 1);
+    }
+}
